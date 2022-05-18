634546e3d3fb8320d4ac4b2bb81d43f4d85a6c9c
gonadr09 <gonzalo_diaz09@hotmail.com>
2022-05-18 12:40:32 -0300

    comprobar nueva palabra

--- js/new-word.js
+++ js/new-word.js
@@ -9,17 +9,38 @@ function addNewWord(){
 
     // funcionamiento input y botones de pantalla add new Word
     const inputText = document.querySelector("#new-word-input");
+    const confirmNewWord = document.querySelector("#confirm-new-word")
     const saveButton = document.querySelector("#new-word-save");
     const cancelButton = document.querySelector("#new-word-cancel");
 
-    cancelButton.addEventListener("click", mainMenu)
-    saveButton.addEventListener("click", () => {
-        if(inputText.value != ""){
-            wordsList.push((inputText.value).toUpperCase())
-            inputText.value = "";
-            createBoardGame()
-            console.log(inputText.value)
-            console.log(wordsList)
+    confirmNewWord.innerHTML = ""
+    let tempInput = ""
+    let expresion = /[A-Z]/i;
+
+    inputText.addEventListener("input", (e) => {  
+        if(e.data == null){
+        } 
+        else if(expresion.test(e.data)){
+            tempInput = inputText.value
+        } else {
+            inputText.value = tempInput
         }
+    })
+
+    cancelButton.addEventListener("click", createBoardGame)
+    saveButton.addEventListener("click", () => {        
+        if(inputText.value != ""){
+            if(inputText.value.length <= 20){
+                wordsList.push((inputText.value).toUpperCase())
+                confirmNewWord.textContent = `Agregaste la palabra: ${(inputText.value).toUpperCase()}`
+                confirmNewWord.classList.remove("color-red")
+                confirmNewWord.classList.add("color-green")
+                inputText.value = "";
+            } else {
+                confirmNewWord.textContent = `La palabra supera los 20 caracteres permitidos`
+                confirmNewWord.classList.add("color-red")
+                confirmNewWord.classList.remove("color-green")
+            }
+        } 
     })
 }
